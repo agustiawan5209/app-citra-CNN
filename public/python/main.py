e45e355889f9aacc5b65925e183119a1ccdc36fa
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-05-15 17:14:55 +0800

    update Main.py

--- public/python/main.py
+++ public/python/main.py
@@ -5,43 +5,43 @@ from skimage.transform import hough_circle
 import sys
 from PIL import Image
 
-
 def ekstrak_ciri(gambar):
-    """
-    Fungsi untuk mengekstrak ciri gambar (warna, tekstur, bentuk, dan pola).
+  """
+  Fungsi untuk mengekstrak ciri gambar (warna, tekstur, bentuk, dan pola).
 
-    Args:
-      gambar: Matriks NumPy yang mewakili gambar.
+  Args:
+    gambar: Matriks NumPy yang mewakili gambar.
 
-    Returns:
-      Kamus yang berisi ciri warna, tekstur, bentuk, dan pola.
-    """
+  Returns:
+    Kamus yang berisi ciri warna, tekstur, bentuk, dan pola.
+  """
 
-    # Konversi gambar ke ruang warna BGR
-    gambar_bgr = cv2.cvtColor(gambar, cv2.COLOR_RGB2BGR)
+  # Konversi gambar ke ruang warna BGR
+  gambar_bgr = cv2.cvtColor(gambar, cv2.COLOR_RGB2BGR)
 
-    # Ekstraksi ciri warna
+  # Ekstraksi ciri warna
+  ciri_warna = {}
+  ciri_warna["rata-rata_warna"] = np.mean(gambar_bgr, axis=(0, 1))
 
-    ciri_warna = np.mean(gambar_bgr, axis=(0, 1))
+  gray_image = cv2.cvtColor(gambar, cv2.IMREAD_GRAYSCALE)
+  # Ekstraksi ciri tekstur
+  ciri_tekstur = {}
+  glcm = graycomatrix(gray_image[:, :, 0], distances=[1], angles=[0], levels=256)
+  ciri_tekstur["GLCM"] = graycoprops(glcm)
 
-    gray_image = cv2.cvtColor(gambar, cv2.IMREAD_GRAYSCALE)
-    # Ekstraksi ciri tekstur
+  # Ekstraksi ciri bentuk
+  ciri_bentuk = {}
+  circles = hough_circle(gray_image[:, :, 0], np.arange(1, 100), 10)
+  ciri_bentuk["circles"] = circles
 
-    glcm = graycomatrix(gray_image[:, :, 0], distances=[1], angles=[0], levels=256)
-    ciri_tekstur = graycoprops(glcm)
+  # Ekstraksi ciri pola
+  ciri_pola = {}
+  ciri_pola["hog"] = hog(gray_image[:, :, 0])
 
-    # Ekstraksi ciri bentuk
-    circles = hough_circle(gray_image[:, :, 0], np.arange(1, 100), 10)
-    ciri_bentuk = circles
+  # Gabungkan ciri-ciri
+  ciri = {**ciri_warna, **ciri_tekstur, **ciri_bentuk, **ciri_pola}
 
-    # Ekstraksi ciri pola
-    ciri_pola = {}
-    ciri_pola["hog"] = hog(gray_image[:, :, 0])
-
-    # Gabungkan ciri-ciri
-    ciri = ciri_warna, ciri_tekstur, ciri_bentuk, ciri_pola
-
-    return ciri
+  return ciri
 
 
 if __name__ == "__main__":
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     file_path = Image.open(image_path).filename
     image = cv2.imread(image_path)
     ciri = ekstrak_ciri(image)
-    print(",".join(str(x) for x in ciri))
+    print(ciri)
